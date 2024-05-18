7ffaaeda5685d19d846ad109f4d2061e23cd805c
iamkhadka46 <iam.khadka46@gmail.com>
2024-05-18 14:41:11 +0200

    Data Ingestion Pipeline created

---
+++ src/summarizer_bot/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,15 @@
+from summarizer_bot.config.configuration import ConfigurationManager
+from summarizer_bot.components.data_ingestion import DataIngestion
+from summarizer_bot.logging import logger
+
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
